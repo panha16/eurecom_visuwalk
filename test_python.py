73ac79faff403431a3ff51e62a3b6bb9f01fbe71
ung <ung@eurecom.frm>
2022-10-29 15:13:19 +0200

    refined hough transform,add slowed line video

--- test_python.py
+++ test_python.py
@@ -11,20 +11,18 @@ while video.isOpened():
 	yes,frame = video.read()		#extracting video frames
 	if yes == True :
 		frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)		#converting frames to gray and using canny edge detection with a high treshold
-		edges = cv2.Canny(frame,50,100)
-		lines = cv2.HoughLinesP(edges,1,np.pi/180,200)		#last 3 parameters : treshold,minLineLength,maxLineGap
+		edges = cv2.Canny(frame,50,200)
+		lines = cv2.HoughLinesP(edges,20,np.pi/180,200)		#last 3 parameters : treshold,minLineLength,maxLineGap
 		for line in lines :
 			x1,y1,x2,y2 = line[0]
 			cv2.line(frame,(x1,y1),(x2,y2),(255,0,0),3)
 		#alors ça marche bien mais quand c'est courbe et bah il reconnait rien
 		cv2.imshow('hough tranform',frame)
-		# contours,hierarchy = cv2.findContours(canny,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-		# print("numbers of objects = %d" % (len(contours)))
-		# testdraw = cv2.drawContours(frame, contours, -1, (0,255,0), 5)
-		# cv2.imshow('image we draw on',testdraw)
 
-		if cv2.waitKey(25) & 0xFF == ord('q') :				#pressing q will exit the video player
+		if cv2.waitKey(25) == ord('q') :				#pressing q will exit the video player
 			break
+		if cv2.waitKey(40) == ord('p') :				#pressing p will pause the video
+			cv2.waitKey(0)	
 
 	else :
 		break
