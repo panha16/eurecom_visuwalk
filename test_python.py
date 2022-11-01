830c9944a1ac533fe1308edb0167a72ed8e2b2c5
ung <ung@eurecom.frm>
2022-11-01 17:08:56 +0100

    split image in 2 images, left and right side

--- test_python.py
+++ test_python.py
@@ -11,15 +11,21 @@ while video.isOpened():
 	yes,frame = video.read()		#extracting video frames
 	height = frame.shape[0]
 	width = frame.shape[1]
-	print(width,height)
 	if yes == True :
 		frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)		#converting frames to gray and using canny edge detection with a high treshold
 		edges = cv2.Canny(frame,50,200)
+		left_side =edges[0:int(width/2),0:int(height/2)]
+		right_side = edges[int(width/2):width,int(height/2):int(height)]
+		cnt,hierarchy = cv2.findContours(edges,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)		#counting the numbers of contours 
+
 		lines = cv2.HoughLinesP(edges,20,np.pi/180,200)		#last 3 parameters : treshold,minLineLength,maxLineGap
 		for line in lines :
 			x1,y1,x2,y2 = line[0]
 			cv2.line(frame,(x1,y1),(x2,y2),(255,0,0),3)
 			cv2.line(frame,(round(width/2),0),(round(width/2),2000),(0,255,0),5)		#drawing the middle line
+			print(len(cnt))
+
+
 			#alors maintenant je veux compter les objets : si y en a plus à droite du centre qu'à gauche, c'est qu'il faut aller à droite!!
 		cv2.imshow('hough tranform',frame)
 
