bc8556f0e066338a130f185ad29b061dec671d71
marze <lou.marze@eurecom.fr>
2023-01-17 11:47:45 +0000

    Replace Audio_part.py

--- soundproc/Audio_part.py
+++ soundproc/Audio_part.py
@@ -3,37 +3,32 @@ from scipy.io.wavfile import write
 import pyaudio
 import random
 
+ # Generate the sine wave
+sample_rate = 44100 # fs
+# Changing the number of samples changes the frequency
+duration = 0.3
+array = np.arange(duration*sample_rate)
+zero = np.zeros(int(duration*sample_rate), dtype= float).astype(np.float32)
+
 for i in range (10):
     angle = random.uniform(-70,70)
-
-
+    
     if angle > 0 :
 # Set the amplitude and frequency
-        A_right = 0.08*np.sqrt(angle)
-        f_right = 3.8*angle + 230
-        A_left = 0
-        f_left = 1
+        A = 0.08*np.sqrt(angle)
+        f = 3.8*angle+ 230
+        samples = A*(np.sin(2*np.pi*array*f/sample_rate)).astype(np.float32)
+        # Save stereo samples
+        stereo_samples = np.column_stack((zero, samples))
 
     else : 
-        A_right = 0
-        f_right = 1
-        A_left = 0.08*np.sqrt(np.abs(angle))
-        f_left = 3.8*(-angle) + 230
-        
-
-    # Generate the sine wave
-    sample_rate = 44100 # fs
-    # Changing the number of samples changes the frequency
-    duration = 0.5
-    array = np.arange(duration*sample_rate) #here we can change the last argument if we want shorter frames but it also changes the frequency
-    samples_right = (np.sin(2*np.pi*array*f_right/sample_rate)).astype(np.float32)
-    samples_right *= A_right
-    samples_left = (np.sin(2*np.pi*array*f_left/sample_rate)).astype(np.float32)
-    samples_left *= A_left
-
-    # Save stereo samples
-    stereo_samples = np.column_stack((samples_left, samples_right))
-
+        A = 0.08*np.sqrt(-angle)
+        f = 3.8*(-angle) + 230
+        samples = A*(np.sin(2*np.pi*array*f/sample_rate)).astype(np.float32)
+        # Save stereo samples
+        stereo_samples = np.column_stack((samples, zero))
+       
+    print(stereo_samples)
     # Save the sine wave to a WAV file
     write("sine_wave.wav", sample_rate, stereo_samples)
 
@@ -44,4 +39,5 @@ for i in range (10):
     stream.close()
     p.terminate()
     i += 1
+
 
