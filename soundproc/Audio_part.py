3af18dadd69b5166fbcc3c19d18a9cea380a852e
marze <lou.marze@eurecom.fr>
2023-01-17 11:07:14 +0000

    Replace Audio_part.py

--- soundproc/Audio_part.py
+++ soundproc/Audio_part.py
@@ -2,8 +2,6 @@ import numpy as np
 from scipy.io.wavfile import write
 import pyaudio
 import random
-import time
-
 
 for i in range (10):
     angle = random.uniform(-70,70)
@@ -24,11 +22,10 @@ for i in range (10):
         
 
     # Generate the sine wave
-    Freq_R = []
-    Freq_L = []
     sample_rate = 44100 # fs
     # Changing the number of samples changes the frequency
-    array = np.linspace(0,sample_rate,44100) #here we can change the last argument if we want shorter frames but it also changes the frequency
+    duration = 0.5
+    array = np.arange(duration*sample_rate) #here we can change the last argument if we want shorter frames but it also changes the frequency
     samples_right = (np.sin(2*np.pi*array*f_right/sample_rate)).astype(np.float32)
     samples_right *= A_right
     samples_left = (np.sin(2*np.pi*array*f_left/sample_rate)).astype(np.float32)
