b2a2fa163d0b989c03febd198077677a6f1e55ac
Hamza Parnica <hamza.parnica@eurecom.fr>
2023-01-25 01:52:27 +0100

    looped sound generation

--- rasp_prod/soundgen_rasp.py
+++ rasp_prod/soundgen_rasp.py
@@ -11,25 +11,28 @@ def generate_audio(angle):
     array = np.arange(duration*sample_rate)
     zero = np.zeros(int(duration*sample_rate), dtype= float).astype(np.float32)
 
-    
-    if angle > 0 and angle != 100:
-# Set the amplitude and frequency
-        A = 0.08*np.sqrt(angle)
-        f = 3.8*angle+ 230
-        samples = A*(np.sin(2*np.pi*array*f/sample_rate)).astype(np.float32)
-        # Save stereo samples
-        stereo_samples = np.column_stack((zero, samples))
-
-    elif angle < 0 and angle != 100: 
-        A = 0.08*np.sqrt(-angle)
-        f = 3.8*(-angle) + 230
-        samples = A*(np.sin(2*np.pi*array*f/sample_rate)).astype(np.float32)
-        # Save stereo samples
-        stereo_samples = np.column_stack((samples, zero))
-       
     p = pyaudio.PyAudio()
     stream = p.open(format=pyaudio.paFloat32, channels=2, rate=sample_rate, output=True)
-    stream.write(stereo_samples.tobytes())
+
+    while(True):
+        if angle != 100:
+            if angle > 0:
+        # Set the amplitude and frequency
+                A = 0.08*np.sqrt(angle)
+                f = 3.8*angle+ 230
+                samples = A*(np.sin(2*np.pi*array*f/sample_rate)).astype(np.float32)
+                # Save stereo samples
+                stereo_samples = np.column_stack((zero, samples))
+
+            elif angle < 0:
+                A = 0.08*np.sqrt(-angle)
+                f = 3.8*(-angle) + 230
+                samples = A*(np.sin(2*np.pi*array*f/sample_rate)).astype(np.float32)
+                # Save stereo samples
+                stereo_samples = np.column_stack((samples, zero))
+
+            stream.write(stereo_samples.tobytes())
+
     stream.stop_stream()
     stream.close()
     p.terminate()
